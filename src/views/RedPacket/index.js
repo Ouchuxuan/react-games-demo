ac1625e6232d9956defc1fbabda2a125b10fadb8
欧楚炫 <ocx7793709@hotmail.com>
2021-06-30 21:36:29 +0800

    修复useCallback的bug

--- src/views/RedPacket/index.js
+++ src/views/RedPacket/index.js
@@ -13,25 +13,25 @@ const imgSourceList = [{
   img: pkg1,
   x: 0,
   y: 0,
-  speed: 1,
+  speed: 0.04,
   id: 0
 }, {
   img: pkg2,
   x: 60,
   y: 0,
-  speed: 1.8,
+  speed: 0.4,
   id: 1
 }, {
   img: pkg3,
   x: 120,
   y: 0,
-  speed: 0.8,
+  speed: 0.2,
   id: 2
 }, {
   img: pkg4,
   x: 180,
   y: 0,
-  speed: 2.4,
+  speed: 0.3,
   id: 3
 }, {
   img: pkg5,
@@ -57,8 +57,6 @@ const getImage = (imageSource) => {
 
 
 export default function RedPacket(props) {
-  // 绘图任务队列
-  const taskList = [];
   // 注：如果要向requestAnimateFrame插入新的canvas绘制，可能要用taskList来存储绘制方法
   const container = useRef(null);
   const canvas = useRef(null);
@@ -66,10 +64,10 @@ export default function RedPacket(props) {
   const timeId = useRef(null);
   // 挂载pkgList
   const pkgList = useRef(null);
+  const taskList = useRef([]);
   // 挂在canvasConext
   const ctx = useRef(null)
-  const [canvasWidth, setCanvasWidth] = useState(0)
-  const [canvasHeight, setCanvasHeight] = useState(0)
+  const [canvasSize, setCanvasSize] = useState({ width: 0, height: 0 })
 
   // 清除&暂停动画
   const clearAnimate = () => {
@@ -78,9 +76,9 @@ export default function RedPacket(props) {
     }
   }
 
-  const drawText = (canvasContext, text, x, y) => {
-    canvasContext.font = "46px Georgia";
-    canvasContext.fillText(text, x, y);
+  const drawText = (text, x, y) => {
+    ctx.current.font = "20px Georgia";
+    ctx.current.fillText(text, x, y);
   }
 
   const hitTest = (mouseX, mouseY, callBack = () => { }) => {
@@ -97,13 +95,22 @@ export default function RedPacket(props) {
 
   }
 
-  const movePkg = useCallback(() => {
+  const draw = useCallback((canvasContext) => {
+    canvasContext.clearRect(0, 0, canvasSize.width, canvasSize.height);
+    taskList.current.forEach(task => {
+      task();
+    })
+    clearAnimate()
+    timeId.current = window.requestAnimationFrame(() => { draw(canvasContext) })
+  }, [canvasSize])
+
+  const move = useCallback(() => {
     // 步进
-    const step = 4;
+    const step = 10;
     pkgList.current = pkgList.current.map(item => {
       // 计算当前的y轴位置
       let y = 0;
-      if (item.y <= canvasHeight) {
+      if (item.y <= canvasSize.height) {
         y = item.y + step * item.speed
       }
       return {
@@ -114,67 +121,67 @@ export default function RedPacket(props) {
     pkgList.current.forEach(item => {
       ctx.current.drawImage(item.img, item.x, item.y)
     })
-  }, [canvasHeight]);
+  }, [canvasSize])
 
-  const testTask = useCallback(() => {
-    drawText(ctx.current, '牛逼', canvasWidth / 2-50, canvasHeight / 2);
-  }, [canvasWidth, canvasHeight])
 
-  taskList.push(movePkg)
 
-  const drawing = useCallback((canvasContext) => {
-    canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
-    taskList.forEach(task => {
-      task();
-    })
-    clearAnimate()
-    timeId.current = window.requestAnimationFrame(() => { drawing(canvasContext) })
-  }, [canvasWidth, canvasHeight, taskList])
-
+  taskList.current = [move]
 
   useEffect(() => {
     const { width, height } = container.current.getBoundingClientRect();
     ctx.current = canvas.current.getContext('2d');
-    setCanvasWidth(width);
-    setCanvasHeight(height);
+    setCanvasSize({ width, height })
+  }, [])
 
-
+  const initAnimate = useCallback(()=>{
     Promise.all(imgSourceList.map(item => {
       return getImage(item.img).catch(error => console.error(error))
     })).then(res => {
+
       pkgList.current = res.map((item, index) => {
         return {
           ...imgSourceList[index],
           img: item,
         }
       })
-      pkgList.current.forEach(item => {
-        ctx.current.drawImage(item.img, item.x, item.y)
-      })
+
       clearAnimate()
-      // 进入绘制循环
-      timeId.current = window.requestAnimationFrame(() => { drawing(ctx.current) })
+      timeId.current = window.requestAnimationFrame(() => { draw(ctx.current) })
     })
+  },[draw])
+
+  useEffect(() => {
+    // initAnimate()
+    setTimeout(()=>{
+      initAnimate()
+    },3000)
+    setTimeout(()=>{
+      clearAnimate()
+    },6000)
     // 清除副作用
     return () => {
       clearAnimate()
     }
-  }, [canvasWidth, canvasHeight, drawing])
+  },[initAnimate]);
 
 
-  const handleClick = useCallback(
-    (event) => {
-      const { nativeEvent: { offsetX, offsetY } } = event;
-      hitTest(offsetX, offsetY, (pkgItem) => {
-        console.log('击中', pkgItem);
-        taskList.push(testTask)
-      });
-    }, [taskList, testTask])
+
+
+
+
+
+  const handleClick = (event) => {
+    const { nativeEvent: { offsetX, offsetY } } = event;
+    hitTest(offsetX, offsetY, (pkgItem) => {
+      console.log('击中', pkgItem,canvasSize);
+      taskList.current.push(() => drawText('hahah', 150, 150))
+    });
+  }
 
 
   return (
     <div className='container' ref={container}>
-      <canvas width={canvasWidth} height={canvasHeight} ref={canvas} onClick={handleClick}></canvas>
+      <canvas width={canvasSize.width} height={canvasSize.height} ref={canvas} onClick={handleClick}></canvas>
     </div>
   )
 }
