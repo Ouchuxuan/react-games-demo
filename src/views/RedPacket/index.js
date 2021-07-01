59124b88734d47af66900d8c48a3677384ce040c
欧楚炫 <ocx7793709@hotmail.com>
2021-07-01 18:05:21 +0800

    增加过度倒计时

--- src/views/RedPacket/index.js
+++ src/views/RedPacket/index.js
@@ -1,13 +1,15 @@
-import React, { useRef, useEffect, useState, useCallback } from 'react'
-import './index.scss'
-import pkg1 from '../../assets/1.png'
-import pkg2 from '../../assets/2.png'
-import pkg3 from '../../assets/3.png'
-import pkg4 from '../../assets/4.png'
-import pkg5 from '../../assets/5.png'
+import React, { useRef, useEffect, useState, useCallback } from 'react';
+import './index.72.scss';
+import pkg1 from '../../common/rain/img/1.png';
+import pkg2 from '../../common/rain/img/2.png';
+import pkg3 from '../../common/rain/img/3.png';
+import pkg4 from '../../common/rain/img/4.png';
+import pkg5 from '../../common/rain/img/5.png';
 
 const pkgWidth = 64;
 const pkgHeight = 64;
+
+// 快速版
 
 const imgSourceList = [{
   img: pkg1,
@@ -39,70 +41,66 @@ const imgSourceList = [{
   y: 0,
   speed: 0.15,
   id: 4
-}]
+}];
 
-
-const getImage = (imageSource) => {
+const getImage = imageSource => {
   return new Promise((resolve, reject) => {
     const img = new Image();
     img.src = imageSource;
     img.onload = () => {
-      resolve(img)
-    }
-    img.onerror = (event) => {
-      reject(event)
-    }
-  })
-}
+      resolve(img);
+    };
+    img.onerror = event => {
+      reject(event);
+    };
+  });
+};
 
 
-export default function RedPacket(props) {
+export default function RedPacketRain(props) {
   // 注：如果要向requestAnimateFrame插入新的canvas绘制，可能要用taskList来存储绘制方法
   const container = useRef(null);
   const canvas = useRef(null);
   // 挂载requestAnimateFrameID
+  const animateTimeId = useRef(null);
   const timeId = useRef(null);
   // 挂载pkgList
   const pkgList = useRef(null);
+  // 挂载绘图任务
   const taskList = useRef([]);
-  // 挂在canvasConext
-  const ctx = useRef(null)
-  const [canvasSize, setCanvasSize] = useState({ width: 0, height: 0 })
-
+  // 挂载canvasConext
+  const ctx = useRef(null);
+  const [canvasSize, setCanvasSize] = useState({ width: 0, height: 0 });
+  // 倒计时
+  const [isCountDowning, setIsCountDowning] = useState(true);
+  const [countDownTime, setCountDownTime] = useState(3);
   // 清除&暂停动画
   const clearAnimate = () => {
-    if (timeId.current) {
-      window.cancelAnimationFrame(timeId.current)
+    if (animateTimeId.current) {
+      window.cancelAnimationFrame(animateTimeId.current);
     }
-  }
+  };
 
   const drawText = (text, x, y) => {
-    ctx.current.font = "20px Georgia";
+    ctx.current.font = '20px Georgia';
     ctx.current.fillText(text, x, y);
-  }
+  };
+
 
   const hitTest = (mouseX, mouseY, callBack = () => { }) => {
     for (let i = 0; i < pkgList.current.length; i++) {
       // X 轴命中
-      const hitX = ((mouseX - pkgList.current[i].x) <= pkgWidth) && ((mouseX - pkgList.current[i].x) > 0);
+      const hitX = ((mouseX - pkgList.current[i].x) <= pkgWidth)
+        && ((mouseX - pkgList.current[i].x) > 0);
       // Y 轴命中
-      const hitY = ((mouseY - pkgList.current[i].y) <= pkgHeight) && ((mouseY - pkgList.current[i].y) > 0);
+      const hitY = ((mouseY - pkgList.current[i].y) <= pkgHeight)
+        && ((mouseY - pkgList.current[i].y) > 0);
       if (hitX && hitY) {
         callBack(pkgList.current[i]);
         break;
       }
     }
-
-  }
-
-  const draw = useCallback((canvasContext) => {
-    canvasContext.clearRect(0, 0, canvasSize.width, canvasSize.height);
-    taskList.current.forEach(task => {
-      task();
-    })
-    clearAnimate()
-    timeId.current = window.requestAnimationFrame(() => { draw(canvasContext) })
-  }, [canvasSize])
+  };
 
   const move = useCallback(() => {
     // 步进
@@ -111,77 +109,104 @@ export default function RedPacket(props) {
       // 计算当前的y轴位置
       let y = 0;
       if (item.y <= canvasSize.height) {
-        y = item.y + step * item.speed
+        y = item.y + step * item.speed;
       }
       return {
         ...item,
         y,
-      }
-    })
+      };
+    });
     pkgList.current.forEach(item => {
-      ctx.current.drawImage(item.img, item.x, item.y)
-    })
-  }, [canvasSize])
+      ctx.current.drawImage(item.img, item.x, item.y);
+    });
+  }, [canvasSize]);
 
+  taskList.current = [move];
 
+  const draw = useCallback(canvasContext => {
+    canvasContext.clearRect(0, 0, canvasSize.width, canvasSize.height);
+    taskList.current.forEach(task => {
+      task();
+    });
+    clearAnimate();
+    animateTimeId.current = window.requestAnimationFrame(() => { draw(canvasContext); });
+  }, [canvasSize]);
 
-  taskList.current = [move]
-
-  useEffect(() => {
-    const { width, height } = container.current.getBoundingClientRect();
-    ctx.current = canvas.current.getContext('2d');
-    setCanvasSize({ width, height })
-  }, [])
-
-  const initAnimate = useCallback(()=>{
+  const initAnimate = useCallback(() => {
     Promise.all(imgSourceList.map(item => {
-      return getImage(item.img).catch(error => console.error(error))
+      return getImage(item.img).catch(error => console.error(error));
     })).then(res => {
-
       pkgList.current = res.map((item, index) => {
         return {
           ...imgSourceList[index],
           img: item,
-        }
-      })
+        };
+      });
+      clearAnimate();
+      animateTimeId.current = window.requestAnimationFrame(() => { draw(ctx.current); });
+    });
+  }, [draw]);
 
-      clearAnimate()
-      timeId.current = window.requestAnimationFrame(() => { draw(ctx.current) })
-    })
-  },[draw])
+  const handleClick = event => {
+    const { nativeEvent: { offsetX, offsetY } } = event;
+    hitTest(offsetX, offsetY, pkgItem => {
+      console.log('击中', pkgItem, canvasSize);
+      taskList.current.push(() => drawText('hahah', 150, 150));
+    });
+  };
+
+
 
   useEffect(() => {
-    // initAnimate()
-    setTimeout(()=>{
-      initAnimate()
-    },3000)
-    setTimeout(()=>{
-      clearAnimate()
-    },6000)
-    // 清除副作用
-    return () => {
-      clearAnimate()
+    if (isCountDowning) {
+      if (timeId.current) clearInterval(timeId.current);
+      timeId.current = setInterval(() => {
+        if (countDownTime === 0) {
+          clearInterval(timeId.current);
+          setIsCountDowning(false);
+          return;
+        }
+        setCountDownTime(countDownTime - 1);
+      }, 1000);
     }
-  },[initAnimate]);
+    return () => {
+      clearAnimate();
+    };
+  }, [isCountDowning, countDownTime]);
 
 
+  useEffect(() => {
+    if (!isCountDowning) {
+      const { width, height } = container.current.getBoundingClientRect();
+      ctx.current = canvas.current.getContext('2d');
+      setCanvasSize({ width, height });
+    }
+  }, [isCountDowning]);
 
-
-
-
-
-  const handleClick = (event) => {
-    const { nativeEvent: { offsetX, offsetY } } = event;
-    hitTest(offsetX, offsetY, (pkgItem) => {
-      console.log('击中', pkgItem,canvasSize);
-      taskList.current.push(() => drawText('hahah', 150, 150))
-    });
-  }
+  useEffect(() => {
+    if (canvasSize.width !== 0) {
+      initAnimate();
+    }
+  }, [canvasSize, initAnimate]);
 
 
   return (
-    <div className='container' ref={container}>
-      <canvas width={canvasSize.width} height={canvasSize.height} ref={canvas} onClick={handleClick}></canvas>
+    <div className="red-packets-rain-container" ref={container}>
+      {isCountDowning && (
+        <div className="count-down-time">
+          {countDownTime} S 后红包雨开抢
+        </div>
+      )}
+      {
+        !isCountDowning && (
+          <canvas
+            width={canvasSize.width}
+            height={canvasSize.height}
+            ref={canvas}
+            onClick={handleClick}
+          />
+        )
+      }
     </div>
-  )
+  );
 }
